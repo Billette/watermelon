e624bc608991a7cf299981a9b72c40b6a41e1423
Billette <maxime.billette@gmail.com>
2019-10-09 11:45:39 +0200

    test database users
    just a small set of a database for users

---
+++ src/database/users.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        id: 1,
+        firstName: 'Kevin',
+        lastName: 'Frydman'
+    },
+    {
+        id: 2,
+        firstName: 'Sarah',
+        lastName: 'Lehuby'
+    },
+    {
+        id: 3,
+        firstName: 'Maxime',
+        lastName: 'Billette'
+    }
+];
